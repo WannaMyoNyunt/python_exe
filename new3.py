699d237c1111e62a8e2675f6b9627ed25b51f0b0
Henry Sensei <cwanasumo@gmail.com>
2025-11-30 11:52:07 +0630

    Calculator

---
+++ new3.py
@@ -0,0 +1,19 @@
+"""name = "Wanna"
+age = 26
+print( f"{name} is {age} years old.")"""
+
+"""name = input("What is your name? \n")
+age = input("How old are you? \n")
+print("----------------------------")
+print(f"{name} is {age} years old.")"""
+
+num1 =input("Enter Number 1\n")
+num2 =input("Enter Number 2\n")
+add = int(num1) + int(num2)   
+sub = int(num1) - int(num2)
+mul = int(num1) * int(num2)
+div = int(num1) / int(num2)
+print(f"Your Addition is {add}")
+print(f"Your Subtraction is {sub}")
+print(f"Your Multiplication is {mul}")
+print(f"Your Division is {div}")
